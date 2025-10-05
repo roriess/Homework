bd0bbadec2fdcddc98bb41499cc814827e09f482
Viсtoria Parfyonova <V.V.P.28092006@yandex.ru>
2025-10-05 12:17:41 +0300

    A new enumeration solution

--- src/Homework_4/enumeration.py
+++ src/Homework_4/enumeration.py
@@ -1,46 +1,30 @@
-def is_safe(board, row, col):
-    # Checking the column
-    for i in range(len(board)):
-        if board[i][col]:
-            return False
+import itertools
 
-    # Checking the diagonal from top to bottom and from left to right
-    r = row
-    c = col
-    while r >= 0 and c >= 0:
-        if board[r][c]:
-            return False
-        r -= 1
-        c -= 1
 
-    # Checking the diagonal from top to bottom and from right to left
-    r = row
-    c = col
-    while r >= 0 and c < len(board):
-        if board[r][c]:
-            return False
-        r -= 1
-        c += 1
+def is_safe(queen_positions):
+    num_queens = len(queen_positions)
+
+    for i in range(num_queens):
+        for j in range(i + 1, num_queens):
+            if queen_positions[i] == queen_positions[j] \
+               or abs(queen_positions[i] - queen_positions[j]) == abs(i - j):
+                return False
 
     return True
 
 
 def count_of_solutions(number_of_queens):
-    def generate_boards(row, board):
-        if row == number_of_queens:
-            return [board]
+    '''
+    The index of a queen - column position on the chessboard,
+    and its value - the row position on the chessboard.
+    '''
+    rows = list(range(number_of_queens))
 
-        result = []
-        for col in range(number_of_queens):
-            if is_safe(board, row, col):
-                new_board = [row[:] for row in board]
-                new_board[row][col] = True
-                result.extend(generate_boards(row + 1, new_board))
-        return result
+    possible_positions = itertools.permutations(rows)
 
-    board = [[False] * number_of_queens for _ in range(number_of_queens)]
-    solutions = generate_boards(0, board)
-    return len(solutions)
+    count = sum(1 for pos in possible_positions if is_safe(pos))
+
+    return count
 
 
 print(count_of_solutions(int(input("Enter number of queens: "))))

45e26c0aba226430c95118fda953ccd0b94e686c
Viсtoria Parfyonova <V.V.P.28092006@yandex.ru>
2025-10-12 20:25:23 +0300

    'heapSort.heapSort' is replaced by 'heapSort'

--- src/Homework_6/test_EdgeCases.py
+++ src/Homework_6/test_EdgeCases.py
@@ -2,20 +2,20 @@ from src.Homework_6.heapSort import heapSort
 
 
 def testEmpty():
-    assert heapSort.heapSort([]) == []
+    assert heapSort([]) == []
 
 
 def testOneElm():
-    assert heapSort.heapSort([5]) == [5]
+    assert heapSort([5]) == [5]
 
 
 def testNegativeElm():
-    assert heapSort.heapSort([-1, -2, -3]) == [-3, -2, -1]
+    assert heapSort([-1, -2, -3]) == [-3, -2, -1]
 
 
 def testLargeElm():
-    assert heapSort.heapSort([1000000000, 2000000000]) == [1000000000, 2000000000]
+    assert heapSort([1000000000, 2000000000]) == [1000000000, 2000000000]
 
 
 def testNone():
-    assert heapSort.heapSort(None) is None
+    assert heapSort(None) is None

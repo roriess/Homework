7b7b9bb69eaa31efb0087079e242f4a16c23ecc1
Viсtoria Parfyonova <V.V.P.28092006@yandex.ru>
2025-10-12 20:25:41 +0300

    'heapSort.heapSort' is replaced by 'heapSort'

--- src/Homework_6/test_OrdinaryCases.py
+++ src/Homework_6/test_OrdinaryCases.py
@@ -2,13 +2,13 @@ from src.Homework_6.heapSort import heapSort
 
 
 def test1():
-   assert heapSort.heapSort([0, 10, 8, 9, 3, 4]) == [0, 3, 4, 8, 9, 10]
+   assert heapSort([0, 10, 8, 9, 3, 4]) == [0, 3, 4, 8, 9, 10]
 
 
 def test2():
-   assert heapSort.heapSort([1, 5, 4, 3, 8, 7]) == [1, 3, 4, 5, 7, 8]
+   assert heapSort([1, 5, 4, 3, 8, 7]) == [1, 3, 4, 5, 7, 8]
 
 
 def test3():
-   assert heapSort.heapSort([10, -1, 8]) == [-1, 8, 10]
+   assert heapSort([10, -1, 8]) == [-1, 8, 10]
   
